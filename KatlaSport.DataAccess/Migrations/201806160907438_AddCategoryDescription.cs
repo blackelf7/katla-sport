9c4d46d548d36e92f941d901dd5b8ca2c7bc464a
Tatsiana Kuzmina <tatsiana_kuzmina1@epam.com>
2018-06-17 11:44:36 +0300

    Add documentation for AddCategoryDescription migration.

--- KatlaSport.DataAccess/Migrations/201806160907438_AddCategoryDescription.cs
+++ KatlaSport.DataAccess/Migrations/201806160907438_AddCategoryDescription.cs
@@ -2,6 +2,9 @@ namespace KatlaSport.DataAccess.Migrations
 {
     using System.Data.Entity.Migrations;
 
+    /// <summary>
+    /// Adding category description migration.
+    /// </summary>
     public partial class AddCategoryDescription : DbMigration
     {
         public override void Up()
