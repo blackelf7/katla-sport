d82524fc6424e7631ad56412a7244ba5d67df36a
Tatsiana Kuzmina <tatsiana_kuzmina1@epam.com>
2018-06-26 01:01:46 +0300

    Merge step14. Implement addHiveSection, updateHiveSection, deleteHiveSection methods in HiveSectionService. Add buttons Back, Save, Delete, Undelete, Purge to HiveSectionForm.

--- KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HiveSectionService } from '../services/hive-section.service';
+import { HiveSection } from '../models/hive-section'
 
 @Component({
   selector: 'app-hive-section-form',
@@ -7,8 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiveSectionFormComponent implements OnInit {
 
-  constructor() { }
+	hiveSection = new HiveSection(0, "", "", false, "");
+	existed = false;
+	hiveId: number;
+
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private hiveSectionService: HiveSectionService
+	) { }
 
   ngOnInit() {
+	  this.route.params.subscribe(p => {
+      this.hiveId = p['hiveId'];
+      if (p['id'] === undefined) return;
+      this.hiveSectionService.getHiveSection(p['id']).subscribe(s => this.hiveSection = s);
+      this.existed = true;
+    });
+  }
+  
+  navigateToHiveSections() {
+    this.router.navigate([`/hive/${this.hiveId}/sections`]);
+  }
+
+  onCancel() {
+    this.navigateToHiveSections();
+  }
+  
+  onSubmit() {
+    if (this.existed) {
+      this.hiveSectionService.updateHiveSection(this.hiveSection).subscribe(p => this.navigateToHiveSections());
+    } else {
+      this.hiveSectionService.addHiveSection(this.hiveId, this.hiveSection).subscribe(p => this.navigateToHiveSections());
+    }
+  }
+
+  onDelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, true).subscribe(c => this.hiveSection.isDeleted = true);
+  }
+
+  onUndelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, false).subscribe(c => this.hiveSection.isDeleted = false);
+  }
+
+  onPurge() {
+    this.hiveSectionService.deleteHiveSection(this.hiveSection.id).subscribe(c => this.navigateToHiveSections());
   }
 }
